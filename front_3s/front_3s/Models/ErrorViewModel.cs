bab3bfc766c5cd91d6720607ef7fd594d7c30e83
SENAI SP <senai@info.local>
2023-11-11 11:29:03 -0300

    adicionando o front.

---
+++ front_3s/front_3s/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace front_3s.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
