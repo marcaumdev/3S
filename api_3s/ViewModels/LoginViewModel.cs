9b7228c3138652cf5648f14a898489f1f1e46530
SENAI SP <senai@info.local>
2023-11-11 13:31:58 -0300

    login

---
+++ api_3s/ViewModels/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai_Spmed_webAPI.ViewModels
+{
+    /// <summary>
+    /// Classe responsável pelo modelo de login
+    /// </summary>
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Informe o e-mail do usuário!")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Informe a senha do usuário!")]
+        public string? Senha { get; set; }
+    }
+}
