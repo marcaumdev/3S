f8e286cdd38051d2b75760d1f36f3f6f022dfef8
SENAI SP <senai@info.local>
2023-11-11 12:26:47 -0300

    Inicio Funcionario

---
+++ api_3s/Controllers/AuthController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace api_3s.Controllers;
+
+public class AuthController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ api_3s/Controllers/UsuarioController.cs
@@ -0,0 +1,19 @@
+﻿using api_3s.Application.ViewModel;
+using api_3s.Domains;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api_3s.Controllers;
+
+[ApiController]
+[Route("/api/usuario")]
+public class UsuarioController : Controller
+{
+    [HttpPost]
+    public IActionResult CadastrarUsuario([FromForm] FuncionarioViewModel funcionarioVM)
+    {
+        Funcionario funcionario = new(funcionarioVM.IdTipoUsuario, funcionarioVM.Nome, funcionarioVM.Cpf, 
+            funcionarioVM.Senha, funcionarioVM.Email, funcionarioVM.IdCargo);
+
+        return Ok();
+    }
+}
